8ef623f6c67b10bb47b6f5c279d479e3b579354c
Carol Wang <carolzjwang@gmail.com>
2025-01-18 09:04:06 -0500

    committed all

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/search', methods=['GET'])
+def search():
+    if not request.args['q']:
+        redirect(url_for('home'))
+        return jsonify({"error":"please input a query!", "data": ""})
+    
+    else:
+        redirect(url_for('home'))
+        return jsonify({"error":"", "data": request.args['q']})
+         
+
+if __name__ == "__main__":
+    app.run()
