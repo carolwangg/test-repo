8e3ca26eb2954e383509c547d376d21b45812720
Carol Wang <carolzjwang@gmail.com>
2025-01-18 09:04:39 -0500

    making sure other thing works

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, render_template, request, redirect, url_for
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return "This url is not valid :/"
 
 @app.route('/search', methods=['GET'])
 def search():
